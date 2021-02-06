7a4e203cf44cf7ad2e47a9664f95a33633e64902
HK Yooun <yooun.hk@gmail.com>
2021-02-06 11:57:39 +0900

    Finish tutorial

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur cupiditate dolorem esse eveniet explicabo facere fugiat illo labore laborum, molestias non praesentium quia sequi. Ea iure molestias nesciunt possimus veniam.</span>
+            <span>- Lorem -</span>
+        </div>
+    );
+}
+
+export default About;
